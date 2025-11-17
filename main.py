f4927c9851e1e1ffc13a11118dc7cc494d998165
nozila2024 <nozilarajabboyeva@gmail.com>
2025-11-17 15:50:12 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+#11-misol
+b = "abcdefgh"
+toza = b[0::2]
+print(toza)
+
+#12-misol
+b = "QWERTYUIOP"
+toza = b[::-1]
+toza2 = toza[::2]
+print(toza2)
+
+#13-misol
+b = "abracadabra"
+toza = b[4:8]
+print(toza)
+
+#14-misol
+b = "abcdefghijk"
+print(b[3:3])
+
+#15-misol
+b = "GameOverMission"
+toza = b[4:8]
+print(toza)
+
+#16-misol
+b = "123456789"
+toza = b[4:]
+toza2 = toza[::-1]
+print(toza2)
+
+#17-misol
+b = "indexing"
+toza = b[2:5]
+print(toza)
+
+#18-misol
+b = "slicingIsPowerful"
+toza = b[3:]
+print(toza)
+
+#19-misol
+b = "abcdefghij"
+toza = b[-5:-2]
+print(toza)
+
+#20-misol
+b = "ABCDEFGHIJ"
+toza = b[-2:-8:-1]
+print(toza)
